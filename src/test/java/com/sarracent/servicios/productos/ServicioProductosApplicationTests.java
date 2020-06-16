3270cad623cfbaa618166d2cd6557aa29bd2e356
Damian Sarracent <damian.sarracent@sondeos.com.ar>
2020-06-16 19:55:24 -0300

    first commit

---
+++ src/test/java/com/sarracent/servicios/productos/ServicioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarracent.servicios.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
