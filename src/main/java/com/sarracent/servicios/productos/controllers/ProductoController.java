efe14f8349b1450b82782deff14f83beece17147
Damian Sarracent <damian.sarracent@sondeos.com.ar>
2020-06-25 23:31:03 -0300

    Creando el controlador Producto

---
+++ src/main/java/com/sarracent/servicios/productos/controllers/ProductoController.java
@@ -0,0 +1,29 @@
+package com.sarracent.servicios.productos.controllers;
+
+import com.sarracent.servicios.productos.models.entity.Producto;
+import com.sarracent.servicios.productos.models.services.IProductoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductoController {
+
+    private final IProductoService productoService;
+
+    public ProductoController(IProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @GetMapping("/listar")
+    public List<Producto> listar() {
+        return productoService.findAll();
+    }
+
+    @GetMapping("/ver/{id}")
+    public Producto detalle(@PathVariable Long id) {
+        return productoService.findById(id);
+    }
+}
